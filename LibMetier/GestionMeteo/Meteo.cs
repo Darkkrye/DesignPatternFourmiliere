320630f18fc9e541b0a35744164128c23063901d
Salomé <salome.russier@gmail.com>
2017-06-27 18:17:03 +0200

    meteo fixes 2

--- LibMetier/GestionMeteo/Meteo.cs
+++ LibMetier/GestionMeteo/Meteo.cs
@@ -14,6 +14,8 @@ namespace LibMetier
 
         public override List<PersonnageAbstrait> observers { get; set; }
 
+        private int compteur = 0;
+
         private Random rand;
 
         public Meteo()
@@ -45,11 +47,33 @@ namespace LibMetier
 
         public void ChangementMeteo() {
             var r = rand.Next(0, 100);
+
+            switch (Etat) {
+                case EtatMeteo.Soleil:
+                    Etat =  (r < 5 ) ? EtatMeteo.Orage :
+                            (r > 85) ? EtatMeteo.Pluie : EtatMeteo.Soleil;
+                    break;
+                case EtatMeteo.Pluie:
+                    VerifCompteur(5);
+                    break;
+                case EtatMeteo.Orage:
+                    VerifCompteur(10);
+                    break;
+            }
            
-            // équivalent à if / else if / else
-           Etat =  (r < 10) ? EtatMeteo.Orage :
-                   (r > 65) ? EtatMeteo.Pluie :  EtatMeteo.Soleil;
+           
+           
             Notify();
+        }
+        private void VerifCompteur(int max) {
+            if (compteur < max) {
+                compteur++;
+            }
+            else
+            {
+                compteur = 0;
+                Etat = EtatMeteo.Soleil;
+            }
         }
      
     }
